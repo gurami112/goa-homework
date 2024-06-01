a66777a971e1ad131d4ffdf0c87ed30bd6066635
gurami112 <146463249+gurami112@users.noreply.github.com>
2024-06-01 20:47:38 +0400

    commit

---
+++ projects/html,css,js/why me/script.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".auto-type",{
+    strings: [
+        "Hi i'm Guram Vakhtangashvili.",
+        "Web Developer.", 
+        "Best leader.",
+    ],
+    typeSpeed: 90,
+    backSpeed: 70,
+    loop: true
+})
