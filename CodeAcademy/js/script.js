6696cccea52f1672de2d7b090c47104a8592fca4
gurami112 <146463249+gurami112@users.noreply.github.com>
2024-06-12 03:14:22 +0400

    commit

---
+++ CodeAcademy/js/script.js
@@ -0,0 +1,80 @@
+        // Introduction to Classes
+
+// let halley = {
+//   _name: 'Halley',
+//   _behavior: 0,
+  
+//   get name() {
+//     return this._name;
+//   },
+  
+//   get behavior() {
+//     return this._behavior;
+//   },
+    
+//   incrementBehavior() {
+//     this._behavior++;
+//   }
+// }
+
+
+        // Constructor
+
+
+// class Surgeon {
+//   constructor(name, department) {
+//     this.name = name;
+//     this.department = department;
+//   }
+// }
+
+
+        // Instance
+
+
+// class Surgeon {
+//   constructor(name, department) {
+//     this.name = name;
+//     this.department = department;
+//   }
+// }
+
+// const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+
+// const surgeonJackson  = new Surgeon('Ruth Jackson','Orthopedics' )
+
+
+
+        // Methods
+
+
+// class Surgeon {
+//   constructor(name, department) {
+//     this._name = name;
+//     this._department = department;
+//     this._remainingVacationDays = 20;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+
+//   get department() {
+//     return this._department;
+//   }
+
+//   get remainingVacationDays() {
+//     return this._remainingVacationDays;
+//   }
+
+//   takeVacationDays(daysOff) {
+//     this._remainingVacationDays -= daysOff;
+//   }
+// }
+
+// const surgeonRomero = new Surgeon('Francisco Romero', 'Cardiovascular');
+// console.log(surgeonRomero.name);
+// console.log(surgeonRomero.department);
+// console.log(surgeonRomero.remainingVacationDays);
+
+// surgeonRomero.takeVacationDays(5);
